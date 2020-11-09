4cc571ad9a2d3f1d736f6f979a90d3c2dbb832df
Divu <70699947+imdivu@users.noreply.github.com>
2020-11-09 22:19:53 +0530

    Update time.py

--- fridaybot/modules/time.py
+++ fridaybot/modules/time.py
@@ -1,5 +1,5 @@
 """ It does not do to dwell on dreams and forget to live
-Syntax: .getime"""
+Syntax: .time"""
 
 import asyncio
 import os
@@ -52,10 +52,3 @@ async def _(event):
     await asyncio.sleep(5)
     await event.delete()
 
-
-@friday.on(friday_on_cmd("gtime (.*)"))  # pylint:disable=E0602
-async def _(event):
-    if event.fwd_from:
-        return
-    input_str = event.pattern_match.group(1)
-    logger.info(input_str)  # pylint:disable=E0602
