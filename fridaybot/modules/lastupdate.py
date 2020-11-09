bc324afd5c23a20b74f2c7da1fcee433d1ba41c5
Divu <70699947+imdivu@users.noreply.github.com>
2020-11-09 17:57:41 +0530

    Update lastupdate.py

--- fridaybot/modules/lastupdate.py
+++ fridaybot/modules/lastupdate.py
@@ -44,4 +44,4 @@ async def _(event):
     end = datetime.now()
     (end - start).microseconds / 1000
     uptime = get_readable_time((time.time() - Lastupdate))
-    await event.edit(f"🔰 Friday Userbot Has Been Restarted Or Updated {uptime} Ago !")
+    await event.edit(f"🦁Gujju Robot Has Been Restarted Or Updated {uptime} Ago !")
