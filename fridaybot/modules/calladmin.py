f3ca0f9ced908654ac79c14b155b7a8930638dee
Divu <70699947+imdivu@users.noreply.github.com>
2020-11-09 17:32:52 +0530

    Update calladmin.py

--- fridaybot/modules/calladmin.py
+++ fridaybot/modules/calladmin.py
@@ -3,11 +3,11 @@ from telethon.tl.types import ChannelParticipantsAdmins
 from uniborg.util import friday_on_cmd
 
 
-@friday.on(friday_on_cmd(pattern="admins"))
+@friday.on(friday_on_cmd(pattern="admin"))
 async def _(event):
     if event.fwd_from:
         return
-    mentions = "@admin: **Spam Spotted**"
+    mentions = "@admin : **Spam Spotted**"
     chat = await event.get_input_chat()
     async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
         mentions += f"[\u2063](tg://user?id={x.id})"
