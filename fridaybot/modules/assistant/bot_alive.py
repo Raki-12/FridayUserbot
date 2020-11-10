3f5821f336821b3a9de0ca4192a0f0361798eecd
Divu <70699947+imdivu@users.noreply.github.com>
2020-11-10 13:23:15 +0530

    Update bot_alive.py

--- fridaybot/modules/assistant/bot_alive.py
+++ fridaybot/modules/assistant/bot_alive.py
@@ -16,19 +16,9 @@ from fridaybot import ALIVE_NAME
 from fridaybot.modules import currentversion
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-PM_IMG = "https://telegra.ph/file/22535f8051a58af113586.jpg"
+PM_IMG = "https://telegra.ph/file/a86263ea08437c5b633e7.jpg"
 pm_caption = "➥ **ASSISTANT IS:** `ONLINE`\n\n"
-pm_caption += "➥ **SYSTEMS STATS**\n"
-pm_caption += "➥ **Telethon Version:** `1.15.0` \n"
-pm_caption += "➥ **Python:** `3.7.4` \n"
-pm_caption += "➥ **Database Status:**  `Functional`\n"
-pm_caption += "➥ **Current Branch** : `master`\n"
-pm_caption += f"➥ **Version** : `{currentversion}`\n"
-pm_caption += f"➥ **My Boss** : {DEFAULTUSER} \n"
-pm_caption += "➥ **Heroku Database** : `AWS - Working Properly`\n\n"
-pm_caption += "➥ **License** : [GNU General Public License v3.0](github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n"
-pm_caption += "➥ **Copyright** : By [StarkGang@Github](GitHub.com/StarkGang)\n"
-pm_caption += "[Assistant By Friday 🇮🇳](https://telegra.ph/FRIDAY-06-15)"
+pm_caption += f"➥ **My Boss** : [{DEFAULTUSER}](tg://user?id=1445368987) \n"
 
 # only Owner Can Use it
 @assistant_cmd("alive", is_args=False)
