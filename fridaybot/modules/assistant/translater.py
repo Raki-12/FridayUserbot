99d215209743ad5b493b605e43b8eeaf11f38b28
Divu <70699947+imdivu@users.noreply.github.com>
2020-11-10 13:37:32 +0530

    Update translater.py

--- fridaybot/modules/assistant/translater.py
+++ fridaybot/modules/assistant/translater.py
@@ -1,15 +1,3 @@
-#    Copyright (C) Midhun KM 2020
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
 import emoji
@@ -36,7 +24,7 @@ async def _(event):
     translated = translator.translate(text, dest=lan)
     after_tr_text = translated.text
     output_str = (
-        f"**Translated By Friday Assistant Bot** \n"
+        f"**Translated By Watari Transaction Company Ltd.** \n"
         f"Source {translated.src} \nTranslation {lan} \nWhat I Can Translate From This {after_tr_text}"
     )
     try:
