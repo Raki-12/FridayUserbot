519028bc01a080e0955f8cf549a2fbf33f719628
Divu <70699947+imdivu@users.noreply.github.com>
2020-11-14 15:26:18 +0530

    Update waify.py

--- fridaybot/modules/waify.py
+++ fridaybot/modules/waify.py
@@ -53,7 +53,7 @@ async def _(event):
             request_url = SEARCH_URL.format(BASE_URL, previous_message_text)
             google_rs_response = requests.get(request_url, allow_redirects=False)
             the_location = google_rs_response.headers.get("Location")
-        await event.edit(".")
+        
         headers = {
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0"
         }
