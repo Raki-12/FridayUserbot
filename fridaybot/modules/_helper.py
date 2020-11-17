263fa8faefc2ff52af1f851a24a32b2cc77d7b9c
Divu <70699947+imdivu@users.noreply.github.com>
2020-11-17 11:13:05 +0530

    Update _helper.py

--- fridaybot/modules/_helper.py
+++ fridaybot/modules/_helper.py
@@ -29,8 +29,8 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = """My Userbot Modules Are Listed Here !\n
-\n• Userbot Owner: L \n• Assistant Bot: @WatariRobot"""
+            help_string = """My Userbot Modules Are Listed Here!\n
+\n• Userbot Owner: L \n• Assistant Bot: {tgbotusername}"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername, help_string
             )
